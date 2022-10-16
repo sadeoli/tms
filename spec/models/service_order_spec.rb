d845930ebb9939e5f099bb452c89e650df743f00
Sade Oliveira <sade.oli95@gmail.com>
2022-10-16 20:24:04 -0300

    Validação Inicial: Ordem de Serviço

--- spec/models/service_order_spec.rb
+++ spec/models/service_order_spec.rb
@@ -58,4 +58,51 @@ RSpec.describe ServiceOrder, type: :model do
             
         end
     end
+
+    describe 'calculate' do
+        it 'cria um cálculo vinculado à ordem de serviço' do
+            # Arrange 
+            transportation_modal = TransportationModal.create!(name: 'Bicicleta', max_distance: 10 , min_distance: 1,
+                max_weight: 8, min_weight: 0, flat_rate: 5, status: :active)
+            service_order = ServiceOrder.create!(pickup_address: 'Rua das Amoras, 52 - Campinas/SP', 
+            product_code: 'CX124060', weight: 5, width: 20, height: 40, depth: 50, 
+            recipient_name: 'Maria Carvalho', recipient_address: 'Rua das Laranjeiras, 15 - Campinas/SP', 
+            recipient_phone: '019985463251', distance: 7)
+            Cost.create!(category: :distance, minimum:5, maximum:10,unit_price:3, transportation_modal:transportation_modal)
+            Cost.create!(category: :weight, minimum:0,maximum:8,unit_price:5, transportation_modal:transportation_modal)
+            Timescale.create!(min_distance:1,max_distance:10,deadline:24,transportation_modal:transportation_modal)
+
+            # Act
+            service_order.calculate
+            result = Calculation.where(service_order:service_order,transportation_modal:transportation_modal).count
+
+            # Assert
+            expect(result).to eq 1
+        end
+
+        it 'não um cálculo caso não haja modalidade ativa' do
+            # Arrange 
+            transportation_modal = TransportationModal.create!(name: 'Bicicleta', max_distance: 10 , min_distance: 1,
+                max_weight: 8, min_weight: 0, flat_rate: 5, status: :inactive)
+            service_order = ServiceOrder.create!(pickup_address: 'Rua das Amoras, 52 - Campinas/SP', 
+            product_code: 'CX124060', weight: 5, width: 20, height: 40, depth: 50, 
+            recipient_name: 'Maria Carvalho', recipient_address: 'Rua das Laranjeiras, 15 - Campinas/SP', 
+            recipient_phone: '019985463251', distance: 7)
+            Cost.create!(category: :distance, minimum:5, maximum:10,unit_price:3, transportation_modal:transportation_modal)
+            Cost.create!(category: :weight, minimum:0,maximum:8,unit_price:5, transportation_modal:transportation_modal)
+            Timescale.create!(min_distance:1,max_distance:10,deadline:24,transportation_modal:transportation_modal)
+
+            # Act
+            service_order.calculate
+            result = Calculation.where(service_order:service_order,transportation_modal:transportation_modal).count
+
+            # Assert
+            expect(result).to eq 0
+        end
+    end
+
+    describe 'close' do
+        it 'atualiza status para entregue no prazo' do            
+        end
+    end
 end
